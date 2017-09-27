cc20eba47fc86c73ae3f6a52c8d4f77aa29c731a
liuliuliu <liu1451242814@outlook.com>
2017-09-27 23:02:36 +0800

    add a list.cpp

---
+++ list.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+struct Node
+{
+	char * data;
+	struct Node *next;
+}
+class List
+{
+private:
+	struct Node *head;
+public:
+	List()
+	{
+		head = new Node;
+		head->next=NULL;
+		head->data=NULL;
+	}
+\\Insert a node before the num i node
+	void insert(int i,char *pdata)
+	{
+		struct Node * p=head;
+		while(i--)
+			p=p->next;
+		Node * node=new Node;
+		node->data=pdata;
+		node->next=p->next;
+		p->next=node;
+	}
+	void delete(int i,char *pdata)
+	{
+		struct Node * p=head;
+		while(--i)
+			p=p->next;
+		strcut Node * t=p->next;
+		p->next=p->next->next;
+		delete t;
+	}
+}
